cab658f27dc8eb70b37afaaa7a9b4c08ad066abe
Joe Combopiano <joecombopiano@Joes-MacBook-Pro.local>
2021-04-27 13:22:47 -0600

    I broke my own site

---
+++ precache-manifest.2552db154906f90733326f044f44a560.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8dc852fb1343d2c267a589e8d3e4dd3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "546936c59d437f67df76",
+    "url": "/static/css/main.df9265e0.chunk.css"
+  },
+  {
+    "revision": "b527ce0abc1ff9389d93",
+    "url": "/static/js/2.3846fed6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3846fed6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "546936c59d437f67df76",
+    "url": "/static/js/main.b00f5179.chunk.js"
+  },
+  {
+    "revision": "c815442ab336b747f527",
+    "url": "/static/js/runtime-main.94a6af97.js"
+  },
+  {
+    "revision": "19d60571672deee8ad08f8fb33fdecad",
+    "url": "/static/media/ie-logo.19d60571.png"
+  },
+  {
+    "revision": "64d390dc93b7f456d6011726e2179c50",
+    "url": "/static/media/insta.64d390dc.png"
+  },
+  {
+    "revision": "687ae324890deae75810ddefbf5476d0",
+    "url": "/static/media/joe.687ae324.jpg"
+  },
+  {
+    "revision": "c68a5248d5bf6bea3ce4b5afc023861f",
+    "url": "/static/media/linkedin.c68a5248.png"
+  },
+  {
+    "revision": "24d2d1e0f1b4ae1b4c20e81d4839a62c",
+    "url": "/static/media/logo.24d2d1e0.svg"
+  },
+  {
+    "revision": "f2667b6edcfbe6a6b9cf8d45c585267c",
+    "url": "/static/media/phone.f2667b6e.png"
+  },
+  {
+    "revision": "c962f761fd202401a3c41b8107b4cac8",
+    "url": "/static/media/pin.c962f761.png"
+  }
+]);
